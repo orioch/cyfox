54f5882a1947ae67d1043facecadfd1afc43549a
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2024-02-09 13:48:22 +0200

    create LoginSection

---
+++ client/src/components/sections/LoginSection/LoginSection.jsx
@@ -0,0 +1,7 @@
+const LoginSection = () => {
+  return (
+    <div className="flex flex-col shadow-lg rounded-xl bg-gray-100 p-10"></div>
+  );
+};
+
+export default LoginSection;
--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="bg-red-200">test</div>
+    <App />
   </React.StrictMode>
 );
 
