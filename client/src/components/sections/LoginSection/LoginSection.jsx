dd272cf58825bd6c9b51a2dd1b5996b1c506b332
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2024-02-09 19:23:01 +0200

    change label to Email

--- client/src/components/sections/LoginSection/LoginSection.jsx
+++ client/src/components/sections/LoginSection/LoginSection.jsx
@@ -7,7 +7,7 @@ const LoginSection = () => {
     <div className="flex flex-col shadow-gray-600 shadow-l rounded-xl bg-gray-300 p-16 gap-10">
       <h1 className="font-semibold mr-auto ml-auto">Login</h1>
       <div className="flex flex-col gap-1">
-        <Input type={INPUT_TYPES.EMAIL} label="Username" />
+        <Input type={INPUT_TYPES.EMAIL} label="Email" />
         <Input type={INPUT_TYPES.PASSWORD} label="Password" />
         <Button className="ml-auto mr-auto">Login</Button>
       </div>
