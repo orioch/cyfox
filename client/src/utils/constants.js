7880430eeebca0d00092ff107438f8e76079d9a5
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2024-02-09 18:10:13 +0200

    create Input compoennt

---
+++ client/src/utils/constants.js
@@ -0,0 +1,10 @@
+export const INPUT_TYPES = {
+  EMAIL: "email",
+  NUMBER: "number",
+  PASSWORD: "password",
+  SEARCH: "search",
+  TEL: "tel",
+  TEXT: "text",
+  TIME: "time",
+  URL: "url",
+};
