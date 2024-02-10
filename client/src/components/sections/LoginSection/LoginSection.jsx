8472b71ec4ee47b95c61c2adb478bd710ca5c7d9
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2024-02-10 18:49:41 +0200

    add sign up button i login section

--- client/src/components/sections/LoginSection/LoginSection.jsx
+++ client/src/components/sections/LoginSection/LoginSection.jsx
@@ -1,8 +1,16 @@
-import { INPUT_TYPES } from "../../../utils/constants";
+import { useCallback } from "react";
+import { INPUT_TYPES, ROUTES } from "../../../utils/constants";
 import Button from "../../atoms/Button/Button";
 import Input from "../../atoms/Input/Input";
+import { useNavigate } from "react-router-dom";
 
 const LoginSection = () => {
+  const navigate = useNavigate();
+
+  const handleSignUpClick = useCallback(() => {
+    navigate(ROUTES.SIGNUP);
+  }, [navigate]);
+
   return (
     <div className="flex flex-col shadow-gray-600 shadow-l animate-in slide-in-from-bottom-12 fade-in duration-500 rounded-xl bg-gray-300 p-16 gap-10">
       <h1 className="font-semibold mr-auto ml-auto">Login</h1>
@@ -11,6 +19,16 @@ const LoginSection = () => {
         <Input type={INPUT_TYPES.PASSWORD} label="Password" />
         <Button className="ml-auto mr-auto">Login</Button>
       </div>
+      <span>
+        New to Cyfox?{" "}
+        <button
+          onClick={handleSignUpClick}
+          className="text-prim-default font-semibold hover:-translate-y-1 hover:scale-110 transition"
+        >
+          Sign Up
+        </button>{" "}
+        here!
+      </span>
     </div>
   );
 };
