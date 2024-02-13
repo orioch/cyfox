5816b95efca60f6f1415c074f9136d7aad3df880
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2024-02-13 23:37:07 +0200

    add timeout to createNewUser

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,10 @@
 import { signup } from "../redux/slices/mockServerSlice";
 
-export const createNewUser = async ({ userData, dispatch }) => {
-  await dispatch(signup(userData));
+export const createNewUser = ({ userData, dispatch }) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      dispatch(signup(userData));
+      resolve();
+    }, Math.random() * (5000 - 1000) + 1000);
+  });
 };
