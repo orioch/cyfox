bf7b6755075720b0e1e5e3235b6b710203400052
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2024-02-14 01:17:05 +0200

    Update HomePage.jsx

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,5 +1,29 @@
+import { useDispatch } from "react-redux";
+import { COLORS } from "../../../utils/constants";
+import Button from "../../atoms/Button/Button";
+import { setToken } from "../../../redux/slices/tokenSlice";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const dispatch = useDispatch();
+
+  const handleLogOut = () => {
+    dispatch(setToken());
+  };
+
+  return (
+    <div className="flex w-full h-full flex-col gap-7 justify-center items-center text-white text-5xl">
+      <h1 className="animate-in slide-in-from-bottom-12 fade-in duration-500">
+        Welcome to the home page!
+      </h1>
+      <Button
+        onClick={handleLogOut}
+        className="animate-in slide-in-from-bottom-12 fade-in duration-1000"
+        color={COLORS.PRIM.DEFAULT}
+      >
+        Log Out
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
