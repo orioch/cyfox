9e1df3a27088cb0bfa175632879633fb6e0e8560
Ori Ochayon <101987673+orioch@users.noreply.github.com>
2024-02-14 02:25:13 +0200

    keep states in storage

--- src/store.js
+++ src/store.js
@@ -1,8 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import mockServerSlice from "./redux/slices/mockServerSlice";
 import tokenSlice from "./redux/slices/tokenSlice";
+import persistStore from "redux-persist/es/persistStore";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  mockServerSlice: persistReducer(
+    { key: "root", storage: storage },
+    mockServerSlice
+  ),
+  tokenSlice: tokenSlice,
+});
 
 export const store = configureStore({
-  reducer: { mockServerSlice, tokenSlice },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
